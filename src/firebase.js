7d46baa56dd32b53464a3a52727f9d998529a42a
HONOKA ANRAKU <rakuraku2563nonnon@gmail.com>
2021-10-28 17:45:41 +0900

    firebaseの設定を追加

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBiZhAIhIIdNBMgld9_F7hFh0rBfuSEz-Y',
+  authDomain: 'drawingmemory-3bcfe.firebaseapp.com',
+  projectId: 'drawingmemory-3bcfe',
+  storageBucket: 'drawingmemory-3bcfe.appspot.com',
+  messagingSenderId: '549654553308',
+  appId: '1:549654553308:web:94883330236e3bc83aca19',
+  measurementId: 'G-5RGLK4PEV3'
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
+export const db = firebase.firestore();
